c51a42451ad5deadb206aaca3277d8a7e93e1b80
katierose9 <kwhitney98@gmail.com>
2018-02-03 01:29:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  `appends a kitten to the end of the kittens array`
+  return kittens.push("ralph")
 }
 destructivelyAppendKitten('Ralph')
